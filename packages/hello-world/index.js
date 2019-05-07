9d5a40ed9f33159a65fcf4b0c44cc530a43729df
Jirat Ki <jirat.ki@wework.com>
2019-05-07 14:01:37 +0800

    fix: 🐛 Add !

--- packages/hello-world/index.js
+++ packages/hello-world/index.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 const changeCase = require('change-case')
 
-const strings = ['Hello', 'World!!']
+const strings = ['Hello', 'World!!!']
 
 function hello() {
   return changeCase.camel(_.join(strings, ', '))
